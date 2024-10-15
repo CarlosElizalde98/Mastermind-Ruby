7e8ce9713ab34a295d284fb5d20e5606a03fac96
Carlos E <github@capitan.simplelogin.com>
2024-10-15 13:11:49 -0500

    Made logic for guessing correctly.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -10,9 +10,11 @@ class GameBoard
     @correct_guesses = {}
     @spaces = []
     @guesses = []
+ 
   end
 
   def print_board
+    puts @correct_guesses.keys.sort
     @guesses.each {|guess|
       puts "|#{guess}|"
     }
@@ -30,19 +32,34 @@ class GameBoard
   end
 
   def guess_calculator(guesses, secret_code)
-    guesses.each_with_index { |guess, index|
-        if guess == secret_code[index]
-          @correct_guesses[index] = "Red"
-        elsif guess != secret_code[index]
-          secret_code.length.times do |num|
-            if guess == secret_code[num] && index != num
-              @correct_guesses[index] = "White"
-            end
-          end
-        else
-          @correct_guesses[index] = ' '
+    reset_correct_guesses
+    correct_guesses = secret_code.slice(0..-1)
+    find_red_guesses(guesses, correct_guesses)
+  end
+
+  def find_red_guesses(guesses, secret_code)
+    secret_code.each_with_index { |guess, index|
+      if guess == guesses[index]
+        @correct_guesses[index] = "Red"
+        secret_code[index] = "Done"
+      end
+      binding.pry
+  }
+    if secret_code.any? {|value| guesses.include?(value)}
+        find_white_guesses(guesses, secret_code)
+    end 
+  end
+
+  def find_white_guesses(guesses, secret_code)
+    secret_code.each_with_index { |correct, index|
+      guesses.length.times do |num|
+        if correct == guesses[num] && index != num
+          @correct_guesses[index] = "White"
+          secret_code[index] = "Done"
+        
         end
-      }
+      end
+    }
   end
 
   def reset_correct_guesses
